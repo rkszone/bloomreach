e931e0c5201b1bfa97c70bdcf41df01f69a65f40
Rahul Kumar Singh <rahul.kumar.singh@nl.abnamro.com>
2022-01-31 09:18:06 +0100

    Added Unit test

---
+++ site/components/src/test/java/org/example/rest/client/CmsEventListenerInitializerTest.java
@@ -0,0 +1,12 @@
+package org.example.rest.client;
+
+import junit.framework.TestCase;
+
+public class CmsEventListenerInitializerTest extends TestCase {
+
+    public void testInit() {
+    }
+
+    public void testDestroy() {
+    }
+}
---
+++ site/components/src/test/java/org/example/rest/client/CmsEventListenerTest.java
@@ -0,0 +1,9 @@
+package org.example.rest.client;
+
+import junit.framework.TestCase;
+
+public class CmsEventListenerTest extends TestCase {
+
+    public void testHandleEvent() {
+    }
+}
